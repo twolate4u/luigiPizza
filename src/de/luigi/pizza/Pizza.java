7156a85e10d9346399315a59a214ad03d824674d
twolate4u <soeren.messinger@gmail.com>
2021-07-05 23:00:18 +0200

    pizza obj continued.

--- src/de/luigi/pizza/Pizza.java
+++ src/de/luigi/pizza/Pizza.java
@@ -7,12 +7,12 @@ public class Pizza {
 
     private double cost;
     private String Name;
-//    maximale Anzahl von Belägen pro Pizza
+//    maximale Anzahl von Belägen pro Pizza. inkl. Sauce.
     private int maxBelag = 8;
 //    maximale Anzahl von Saucen pro Pizza
     private int maxSauce = 1;
-    private List<Belag> belagList = new ArrayList<Belag>(maxBelag);
-    private List<Sauce> sauceList = new ArrayList<Sauce>(maxSauce);
+    private List<Belag> belagList = new ArrayList<Belag>();
+    private List<Sauce> sauceList = new ArrayList<Sauce>();
 
 //    setter
     public void setCost(double cost) {
@@ -31,22 +31,14 @@ public class Pizza {
         this.maxSauce = maxSauce;
     }
 
-    public void setBelagList(List<Belag> belagList) {
-        this.belagList = belagList;
-    }
+    public void setBelagList(List<Belag> belagList) { this.belagList = belagList; }
 
-    public void setSauceList(List<Sauce> sauceList) {
-        this.sauceList = sauceList;
-    }
+    public void setSauceList(List<Sauce> sauceList) { this.sauceList = sauceList; }
 
 //    getter
-    public double getCost() {
-        return cost;
-    }
+    public double getCost() { return cost; }
 
-    public String getName() {
-        return Name;
-    }
+    public String getName() { return Name; }
 
     public int getMaxBelag() {
         return maxBelag;
@@ -68,24 +60,55 @@ public class Pizza {
 //    adds a Belag to the pizza.
 //    returns 0 if successful, 1 if the maximum off toppings is reached
     public int addBelag(Belag belag) {
-
+        if ((belagList.size() + sauceList.size()) >= maxBelag) {
+            return 1;
+        }
+        else {
+            belagList.add(belag);
+            return 0;
+        }
     }
 
 //    adds a Sauce to the pizza.
 //    returns 0 if successful, 1 if the maximum off toppings is reached, 2 if there is already a sauce on the pizza.
+//    returns 1 if both errors occur.
     public int addSauce(Sauce sauce) {
-
+        if ((belagList.size() + sauceList.size()) >= maxBelag) {
+            return 1;
+        }
+        else if (sauceList.size() >= maxSauce) {
+            return 2;
+        }
+        else {
+            sauceList.add(sauce);
+            return 0;
+        }
     }
 
-//    removes a Belag from the pizza.
-//    returns 0 if successful, 1 if there were none
-    public int removeBelag(Belag belag) {
-
+//    removes a Belag from the pizza. Maybe overload for removal via index?
+//    returns true if sucessful, false if belag not in list
+    public boolean removeBelag(Belag belag) {
+        return belagList.remove(belag);
     }
 
-//    removes a Sauce from the pizza.
-//    returns 0 if successful, 1 if there was none.
-    public int removeSauce(Sauce sauce) {
+//    does this even work or do i need a temp object?
+    public boolean removeBelag(int belag) {
+        if (belagList.remove(belag) == belagList.get(belag)) {
+            return true;
+        }
+        return false;
+    }
 
+//    removes a Sauce from the pizza. Maybe overload for removal via index?
+//    returns true if sucessful, false if belag not in list
+    public boolean removeSauce(Sauce sauce) {
+        return belagList.remove(sauce);
+    }
+
+    public boolean removeSauce(int sauce) {
+        if (belagList.remove(sauce) == belagList.get(sauce)) {
+            return true;
+        }
+        return false;
     }
 }
