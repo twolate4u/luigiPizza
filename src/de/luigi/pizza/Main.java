1f8d49a7ce1de6c7dac484e3b3b506ff3e633119
Liesa <86413479+Lizzy88@users.noreply.github.com>
2021-07-07 16:12:09 +0200

    Update Main
    Go to checkout

--- src/de/luigi/pizza/Main.java
+++ src/de/luigi/pizza/Main.java
@@ -2,15 +2,9 @@ package de.luigi.pizza;
 
 import java.util.Scanner;
 
-import static java.awt.SystemColor.menu;
-
 public class Main {
 
     public static void main(String[] args) {
-        Auswahl auswahl = new Auswahl();
-
-        System.out.println("zwiebeln = " + auswahl.zwiebeln);
-
         welcomeMenu();
     }
 
@@ -24,38 +18,106 @@ public class Main {
         System.out.println("**********Luigis-Pizza**********");
 
         String orderName;
-        orderName = in.next().trim();
+        orderName = in.nextLine().trim();
         while (orderName.length() > 31) {
             System.out.println("Der Name ist zu lang. Bitte versuchen Sie es mit einem kürzeren Namen: ");
-            orderName = in.next().trim();
+            orderName = in.nextLine().trim();
         }
 
-        System.out.println("Bitte wähle aus"); //Was soll bestellt werden?
+//        System.out.println("Bitte wähle aus"); //Was soll bestellt werden?
 
         Order order = new Order(orderName);
         orderMenu(orderName, order);
     }
 
+
+
     private static void orderMenu(String orderName, Order order) {
-        /*hier müsste eine Pizza erstellt werden und dann die Zutatenauswahl angezeigt werden*/
-        System.out.println();
-        System.out.println("Bestellung: " + orderName);
-        System.out.println();
-        System.out.println("Pizza: " + order.getOrderList().);
+//        neue Pizza wird erstellt und der Bestellung hinzugefügt. So lange wir keine Änderungen zulassen reicht das so aus.
+//        sonst müssen wir auch die Möglichkeit einplanen eine bestehende Pizza zu laden.
+        neuePizza:
+        while (true) {  //schleife die ermöglicht mehr als eine pizza zu erstellen.
+            Pizza pizza = new Pizza();
+            order.getOrderList().add(pizza);
 
+            Scanner in = new Scanner(System.in);
+            String command = "";
+//        Menü:
+            System.out.println();
+            System.out.println("Erstelle eine neue Pizza für: " + orderName);
+            System.out.println("Pizza No.: " + (order.getOrderList().indexOf(pizza)+1)+"\n"); // +1 weil sonst die erste Pizza 0 ist.
+            System.out.println("Die Zutatenliste zeigt an, aus welchen Toppings Sie wählen können.\n");
+            Auswahl.printAuswahl();
+            System.out.println("Bitte geben Sie nacheinander eine Soße und bis zu acht weitere Zutaten für Ihre Pizza ein.");
+            System.out.println("Mit <Zutaten> können Sie die Zutatenliste erneut aufrufen. \nMit dem Befehl <fertig> schließen Sie die Auswahl ab.");
+
+            while (true) {
+                command = in.nextLine().trim().toLowerCase();
+                if (command.equals("zutaten")) {
+                    Auswahl.printAuswahl();
+                }
+                else if (command.equals("fertig")) {
+                    break;
+                }
+                else if (Auswahl.inAuswahl(command)) {     //ist die zutat vorhanden?
+                    Belag Wahl = Auswahl.getBelag(command);
+                    if (Wahl instanceof Zutat) {        //das hier ist absurd. das muss anders gehen... instanceof erkennt den richtigen objekttyp aber die überladene methode nicht
+                        if (pizza.addBelag((Zutat) Wahl) == 1) {
+                            System.out.println("Ihre Pizza hat bereits 8 Zutaten. Falls noch nicht geschehen, wählen Sie eine Sauce und schliessen Sie die Pizza mit <fertig>.");
+                            continue;
+                        }
+                    }
+                    else if (Wahl instanceof Sauce) {
+                        if (pizza.addBelag((Sauce) Wahl) == 2) {
+                            System.out.println("Ihre Pizza hat bereits eine Sauce. Wählen Sie noch bis zu 8 Zutaten und schliessen Sie die Pizza mit <fertig>.");
+                            continue;
+                        }
+                    }
+                }
+                else {
+                    System.out.println("ungültiger Befehl. <Zutaten> für eine Liste der Zutaten. <Fertig> zum Schliessen der Pizza.");
+                }
+            }
+            System.out.println("Möchten Sie eine weitere Pizza zu Ihrer Bestellung hinzufügen <Ja> <Nein>?");
+
+            while (true) {
+                command = in.nextLine().trim().toLowerCase();
+
+                if (command.equals("nein")) {
+                    System.out.println("Möchten Sie die Bestellung abschicken <Ja> <Nein>?");
+                    if (command.equals("ja")) {
+                        System.out.println("Sie werden zur Kasse weitergeleitet");
+                        //AUSGABE PREIS
+                        System.out.println("Die Pizza" + order.getOrderList().indexOf(pizza)+1 + "kostet" + "XXXX Euro"+ \ "Bitte bezahlen Sie via PayPal");;
+                        // AUFFORDERUNG BEZAHLUNG
+                        System.out.println("Bitte geben sie ");
+                    }
+                    else if (command.equals("nein")) {
+                    }
+                    //ZURÜCK ZUM BEARBEITEN
+                    ///continue ;
+                    else {
+                        System.out.println("ungültiger Befehl. <ja>: Bestellung abschicken. <nein>: Zurück zur Pizza.");
+                        break;
+                    }
+                }
+                else if (command.equals("ja")) {
+                    continue neuePizza; //springt an den Anfang und erstellt neue Pizza.
+                }
+                else {
+                    System.out.println("ungültiger Befehl. <ja> für eine weitere Pizza. <nein> zum Abschicken der Bestellung.");
+                }
+            }
+        }
     }
-
 }
 
    /* int befehl = befehlParsen("Bitte geben Sie eine Ganzzahl ein: ");
         System.out.println("befehl = " + befehl);
-
                 }
-
 private static int befehlParsen(String eingabeText) {
         Scanner scanner = new Scanner(System.in);
         int befehlscode = -1;
-
         do {
         System.out.print(eingabeText);
         String befehl = scanner.nextLine();
